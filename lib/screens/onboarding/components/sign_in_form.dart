8e5f9149b41b455eba88063ffe416ded8c168719
Nicolas Lépinay <nicolas@nosoft.io>
2023-05-09 17:12:16 +0200

    Success/Fail animations

--- lib/screens/onboarding/components/sign_in_form.dart
+++ lib/screens/onboarding/components/sign_in_form.dart
@@ -1,72 +1,209 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:rive/rive.dart';
 
-class SignInForm extends StatelessWidget {
+class SignInForm extends StatefulWidget {
   const SignInForm({
     super.key,
   });
 
   @override
+  State<SignInForm> createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool isShowLoading = false;
+  bool isShowConfetti = false;
+
+  late SMITrigger check; // Success animation
+  late SMITrigger error; // Error animation
+  late SMITrigger reset;
+  late SMITrigger confetti;
+
+  StateMachineController? getRiveController(Artboard artboard) {
+    StateMachineController? controller =
+        StateMachineController.fromArtboard(artboard, "State Machine 1");
+
+    artboard.addController(controller!);
+    return controller;
+  }
+
+  void signIn(BuildContext context) {
+    // Shows the loading animation
+    setState(() {
+      isShowLoading = true;
+      isShowConfetti = true;
+    });
+    Future.delayed(
+      const Duration(seconds: 1),
+      () {
+        if (_formKey.currentState!.validate()) {
+          // Shows the success animation
+          check.fire();
+          Future.delayed(
+            const Duration(seconds: 2),
+            () {
+              setState(() {
+                isShowLoading = false;
+              });
+              confetti.fire();
+              // TODO: Navigate to next screen
+            },
+          );
+        } else {
+          // Shows the error animation
+          error.fire();
+          Future.delayed(
+            const Duration(seconds: 2),
+            () {
+              setState(() {
+                isShowLoading = false;
+              });
+            },
+          );
+        }
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "Adresse email",
-            style: TextStyle(color: Colors.black54),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 14, bottom: 26),
-            child: TextFormField(
-              decoration: InputDecoration(
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  child: SvgPicture.asset("assets/icons/email.svg"),
-                ),
+    return Stack(
+      children: [
+        Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Adresse email",
+                style: TextStyle(color: Colors.black54),
               ),
-            ),
-          ),
-          const Text(
-            "Mot de passe",
-            style: TextStyle(color: Colors.black54),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 8, bottom: 16),
-            child: TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  child: SvgPicture.asset("assets/icons/password.svg"),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, bottom: 24),
-            child: ElevatedButton.icon(
-              onPressed: () {},
-              icon: const Icon(
-                CupertinoIcons.arrow_right,
-                color: Color(0xFFFE0037),
-              ),
-              label: const Text("Se connecter"),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFFF77D8E),
-                minimumSize: const Size(double.infinity, 56),
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(25),
-                    bottomRight: Radius.circular(25),
-                    bottomLeft: Radius.circular(25),
+              Padding(
+                padding: const EdgeInsets.only(top: 14, bottom: 26),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "";
+                    }
+                    return null;
+                  },
+                  onSaved: (email) {},
+                  decoration: InputDecoration(
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: SvgPicture.asset("assets/icons/email.svg"),
+                    ),
                   ),
                 ),
               ),
-            ),
+              const Text(
+                "Mot de passe",
+                style: TextStyle(color: Colors.black54),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8, bottom: 16),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "";
+                    }
+                    return null;
+                  },
+                  onSaved: (password) {},
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: SvgPicture.asset("assets/icons/password.svg"),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, bottom: 24),
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    signIn(context);
+                  },
+                  icon: const Icon(
+                    CupertinoIcons.arrow_right,
+                    color: Color(0xFFFE0037),
+                  ),
+                  label: const Text("Se connecter"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFF77D8E),
+                    minimumSize: const Size(double.infinity, 56),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(25),
+                        bottomRight: Radius.circular(25),
+                        bottomLeft: Radius.circular(25),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
+        ),
+        isShowLoading
+            ? CustomPositioned(
+                child: RiveAnimation.asset(
+                  "assets/RiveAssets/check.riv",
+                  onInit: (artboard) {
+                    StateMachineController? controller =
+                        getRiveController(artboard);
+                    check = controller!.findSMI("Check") as SMITrigger;
+                    error = controller!.findSMI("Error") as SMITrigger;
+                    reset = controller!.findSMI("Reset") as SMITrigger;
+                  },
+                ),
+              )
+            : const SizedBox(),
+        isShowConfetti
+            ? CustomPositioned(
+                child: Transform.scale(
+                  scale: 7,
+                  child: RiveAnimation.asset(
+                    "assets/RiveAssets/confetti.riv",
+                    onInit: (artboard) {
+                      StateMachineController? controller =
+                          getRiveController(artboard);
+                      confetti = controller!.findSMI("Trigger explosion")
+                          as SMITrigger;
+                    },
+                  ),
+                ),
+              )
+            : SizedBox(),
+      ],
+    );
+  }
+}
+
+class CustomPositioned extends StatelessWidget {
+  const CustomPositioned({super.key, required this.child, this.size = 100});
+
+  final Widget child;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: Column(
+        children: [
+          const Spacer(),
+          SizedBox(
+            height: size,
+            width: size,
+            child: child,
+          ),
+          const Spacer(flex: 2),
         ],
       ),
     );
